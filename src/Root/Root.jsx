4b00dccca359231ac1b1a80177648544d33df77b
anikdev711 <anikdev100@gmail.com>
2023-10-07 20:03:13 +0600

    footer added

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 // import Home from "../pages/Home/Home";
 import Navbar from "../components/Header/Navbar";
+import Footer from "../components/Footer/Footer";
 
 
 const Root = () => {
@@ -8,6 +9,7 @@ const Root = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
