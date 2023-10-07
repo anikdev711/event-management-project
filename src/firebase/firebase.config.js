541be6888c52431541cc7593529d0d25049e3d34
anikdev711 <anikdev100@gmail.com>
2023-10-07 18:53:51 +0600

    event management project created

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqwk583h-m21Uvsufk_rL4DraTWzMxEdA",
+  authDomain: "event-management-project-b69e4.firebaseapp.com",
+  projectId: "event-management-project-b69e4",
+  storageBucket: "event-management-project-b69e4.appspot.com",
+  messagingSenderId: "938905209223",
+  appId: "1:938905209223:web:febd0db2ff902898e0eb22"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
