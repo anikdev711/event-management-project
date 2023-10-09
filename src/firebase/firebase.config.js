c62f6da41e460c1e4423c208530d1c21059e6d5b
anikdev711 <anikdev100@gmail.com>
2023-10-09 18:00:39 +0600

    update firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqwk583h-m21Uvsufk_rL4DraTWzMxEdA",
-  authDomain: "event-management-project-b69e4.firebaseapp.com",
-  projectId: "event-management-project-b69e4",
-  storageBucket: "event-management-project-b69e4.appspot.com",
-  messagingSenderId: "938905209223",
-  appId: "1:938905209223:web:febd0db2ff902898e0eb22"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
